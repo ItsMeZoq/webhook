9c1e3bcd3649c4818ad3f5aace72f1135ef197e5
ItsMeZoq <147277833+ItsMeZoq@users.noreply.github.com>
2025-09-08 10:22:02 +0200

    cli.py

--- cli.py
+++ cli.py
@@ -4,6 +4,7 @@ import app
 parser = argparse.ArgumentParser(description="Easy google chat sending")
 
 parser.add_argument("--msg", required=True, help="msg sends messege to google chat")
+parser.add_argument("--url_name", required=True, help="its a key of url from config.json")
 args = parser.parse_args()
 
-app.send_msg(args.msg)
+app.send_msg(args.msg, args.url_name)
